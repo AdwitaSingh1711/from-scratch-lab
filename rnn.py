d8596835c7cb6b77ec0ab6f16f7afe7148c0589a
AdwitaSingh1711 <108565358+AdwitaSingh1711@users.noreply.github.com>
2025-06-08 01:32:40 +0530

    added tests

--- rnn.py
+++ rnn.py
@@ -41,33 +41,13 @@ class RNN(nn.Module):
 
       hidden_states.append(ht)
       ot = self.co + ht @ self.Vo 
-      ot = torch.softmax(ot, dim=1)
+      # ot = torch.softmax(ot, dim=1)
+      # while the network uses softmax computation at this step
+      # we remove it because it is calculated at the loss computation step anyway
+      # redundant calculations may give numeric insntability
       outputs.append(ot)
     
     return torch.stack(outputs),ht
-  
-sentence = """The red-capped parrot (Purpureicephalus spurius) is a species of broad-tailed parrot native to southwest Western Australia. Described by Heinrich Kuhl in 1820, it is classified in its own genus owing to its distinctive elongated beak. Its closest relative is the mulga parrot. It is not easily confused with other parrot species; both adult sexes have a bright crimson crown, green-yellow cheeks, and a distinctive long bill. The wings, back, and long tail are dark green, and the underparts are purple-blue. Found in woodland and open savanna country, the red-capped parrot consumes seeds (particularly of eucalypts), flowers, berries, and occasionally insects. Nesting takes place in tree hollows. Although the red-capped parrot has been shot as a pest, and affected by land clearing, the population is growing and the species is not threatened."""
-words = sentence.lower().split()
-
-vocab = {word : idx for idx, word in enumerate(set(words))}
-inv_vocab = {idx : word for word, idx in vocab.items()}
-vocab_size = len(vocab)
-
-seq_len = len(words)
-batch_size = 1
-
-x = torch.zeros(seq_len-1, batch_size, vocab_size)
-for t in range(seq_len-1):
-  word = words[t]
-  x[t, 0, vocab[word]] = 1
-
-input_size = vocab_size
-hidden_size = 8
-output_size = vocab_size
-
-rnn = RNN(input_size, hidden_size, output_size)
-optimizer = torch.optim.Adam(rnn.parameters(), lr=0.01)
-# loss_fn = nn.CrossEntropyLoss()
 
 def compute_cross_entropy_loss(output, target):
   """
@@ -87,24 +67,69 @@ def compute_cross_entropy_loss(output, target):
 
   return total_loss/target.size(0)
 
-target = torch.tensor([vocab[words[t+1]]for t in range(seq_len-1)])
+def preprocess(sentence):
+  """
+  sentence: input text to be learned from
+  vocab: mapping of tokens->index of all unique tokens
+  inv_vocab: inverse mapping index->tokens (as a sample decoder)
+  target: true labels for the model
+  words: all tokens in sentence
+  """
+  words = sentence.lower().split()
 
-num_epochs = 300
+  vocab = {word : idx for idx, word in enumerate(set(words))}
+  inv_vocab = {idx : word for word, idx in vocab.items()}
+  vocab_size = len(vocab)
 
-for epoch in range(num_epochs):
-  optimizer.zero_grad()
-  output, _ = rnn(x)
-  # output = output.view(-1, vocab_size)
-  output = output.squeeze(1)
+  seq_len = len(words)
+  batch_size = 1
+
+  x = torch.zeros(seq_len-1, batch_size, vocab_size)
+  for t in range(seq_len-1):
+    word = words[t]
+    x[t, 0, vocab[word]] = 1
   
-  # loss = loss_fn(output,target)
-  loss = compute_cross_entropy_loss(output, target)
-  loss.backward()
-  optimizer.step()
+  target = torch.tensor([vocab[words[t+1]]for t in range(seq_len-1)])
+  
+  return x, vocab, inv_vocab, target, words
 
-  if (epoch+1)%10==0:
-    print(f"Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item():.4f}")
+def train(num_epochs, rnn, optimizer, target, x):
+  """
+  num_epochs: no of training loops for the rnn
+  rnn: object of the RNN class
+  optimizer: object of torch.optim class here
+  target: true labels
+  x: input ids stacked as a tensor for the RNN class
+  """
+  for epoch in range(num_epochs):
+    optimizer.zero_grad()
+    output, _ = rnn(x)
+    # output = output.view(-1, vocab_size)
+    output = output.squeeze(1)
+    
+    # loss = loss_fn(output,target)
+    loss = compute_cross_entropy_loss(output, target)
+    loss.backward()
+    optimizer.step()
 
+    if (epoch+1)%10==0:
+      print(f"Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item():.4f}")
+
+sentence = """The red-capped parrot (Purpureicephalus spurius) is a species of broad-tailed parrot native to southwest Western Australia. Described by Heinrich Kuhl in 1820, it is classified in its own genus owing to its distinctive elongated beak. Its closest relative is the mulga parrot. It is not easily confused with other parrot species; both adult sexes have a bright crimson crown, green-yellow cheeks, and a distinctive long bill. The wings, back, and long tail are dark green, and the underparts are purple-blue. Found in woodland and open savanna country, the red-capped parrot consumes seeds (particularly of eucalypts), flowers, berries, and occasionally insects. Nesting takes place in tree hollows. Although the red-capped parrot has been shot as a pest, and affected by land clearing, the population is growing and the species is not threatened."""
+
+x, vocab, inv_vocab, target, words = preprocess(sentence)
+input_size = len(vocab)
+hidden_size = 8
+output_size = len(vocab)
+num_epochs = 300
+batch_size = 1
+
+rnn = RNN(input_size, hidden_size, output_size)
+optimizer = torch.optim.Adam(rnn.parameters(), lr=0.01)
+# loss_fn = nn.CrossEntropyLoss()
+
+print("Training the RNN...")
+train(num_epochs, rnn, optimizer, target,x)
 
 #NEXT WORD#
 with torch.no_grad():
@@ -135,7 +160,7 @@ for _ in range(num_words_to_generate):
         generated_sequence.append(next_word)
         
         # Update context for next prediction
-        next_input = torch.zeros(1, batch_size, vocab_size)
+        next_input = torch.zeros(1, batch_size, len(vocab))
         next_input[0, 0, next_word_idx] = 1
         context = next_input
 
